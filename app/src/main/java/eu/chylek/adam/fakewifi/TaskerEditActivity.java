d341bab46b9ec431ac755f1a4d87bf07cc2e1a45
chylek <1331917+chylek@users.noreply.github.com>
2017-09-21 23:31:34 +0200

    users can change ssid, bssid and mac with tasker

--- app/src/main/java/eu/chylek/adam/fakewifi/TaskerEditActivity.java
+++ app/src/main/java/eu/chylek/adam/fakewifi/TaskerEditActivity.java
@@ -1,27 +1,126 @@
 package eu.chylek.adam.fakewifi;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class TaskerEditActivity extends AppCompatActivity {
-    final static String EXTRA_MASTER = "eu.chylek.adam.fakewifi.EXTRA_MASTER";
+    final static String KEY_MASTER = "eu.chylek.adam.fakewifi.KEY_MASTER";
+    final static String KEY_MAC = "eu.chylek.adam.fakewifi.KEY_MAC";
+    final static String KEY_SSID = "eu.chylek.adam.fakewifi.KEY_SSID";
+    final static String KEY_BSSID= "eu.chylek.adam.fakewifi.KEY_BSSID";
     final static String EXTRA_BUNDLE = "com.twofortyfouram.locale.intent.extra.BUNDLE";
     final static String EXTRA_STRING_BLURB = "com.twofortyfouram.locale.intent.extra.BLURB";
+    Bundle prefBundle = new Bundle();
 
+    private class EditClickListener implements View.OnClickListener {
+        private boolean checkMac = false;
+        private final String title;
+        private final String message;
+        private String value;
+        private String key;
+
+        EditClickListener(boolean isMac, String key, String value, int title, int message) {
+            checkMac=isMac;
+            Resources r = getResources();
+            this.title = r.getString(title);
+            this.message = r.getString(message);
+            this.value = value;
+            this.key = key;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public void onClick(final View view) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(view.getContext(), R.style.Theme_AppCompat_Dialog_Alert);
+            final EditText edittext = new EditText(view.getContext());
+            edittext.setText(value);
+            alert.setMessage(message);
+            alert.setTitle(title);
+
+            alert.setView(edittext);
+
+            alert.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String value = edittext.getText().toString();
+                    if (checkMac && !Utils.checkMacFormat(value)) {
+                        Toast.makeText(view.getContext(), R.string.wrong_mac, Toast.LENGTH_LONG).show();
+                    }
+                    else {
+                        setValue(value);
+                        prefBundle.putString(key, value);
+                        fillDescriptions();
+                    }
+                }
+            });
+
+            alert.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    edittext.setText(value);
+                    dialog.dismiss();
+                }
+            });
+
+            alert.show();
+        }
+    }
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
 
-        final Bundle localeBundle = getIntent().getBundleExtra(EXTRA_BUNDLE);
+        Bundle localeBundle = getIntent().getBundleExtra(EXTRA_BUNDLE);
 
         if (null == savedInstanceState && localeBundle != null) {
-            final boolean state = localeBundle.getBoolean(EXTRA_MASTER, false);
-            ((ToggleButton) findViewById(R.id.masterswitch)).setChecked(state);
+            final boolean state = localeBundle.getBoolean(KEY_MASTER, false);
+            ((SwitchCompat) findViewById(R.id.masterswitch)).setChecked(state);
+            checkKey(KEY_MAC, localeBundle);
+            checkKey(KEY_SSID, localeBundle);
+            checkKey(KEY_BSSID, localeBundle);
+            fillDescriptions();
         }
+
+        attachListeners();
+    }
+
+    private String checkKey(String key, Bundle bundle){
+        String s = bundle.getString(key, "");
+        prefBundle.putString(key, s);
+        return s;
+    }
+
+    private void attachListeners(){
+        findViewById(R.id.mac).setOnClickListener(new EditClickListener(true, KEY_MAC, prefBundle.getString(KEY_MAC), R.string.title_mac, R.string.dialog_mac));
+        findViewById(R.id.ssid).setOnClickListener(new EditClickListener(false, KEY_SSID, prefBundle.getString(KEY_SSID), R.string.title_ssid, R.string.dialog_ssid));
+        findViewById(R.id.bssid).setOnClickListener((new EditClickListener(true, KEY_BSSID, prefBundle.getString(KEY_BSSID), R.string.title_bssid, R.string.dialog_mac)));
+    }
+
+    private void fillDescription(View v, String key, int defaultText){
+        String s = prefBundle.getString(key, "");
+        if ("".equals(s)){
+            ((TextView) v.findViewById(R.id.description)).setText(defaultText);
+        }
+        else {
+            ((TextView) v.findViewById(R.id.description)).setText(s);
+        }
+    }
+    private void fillDescriptions(){
+        fillDescription(findViewById(R.id.mac),KEY_MAC, R.string.summary_mac);
+        fillDescription(findViewById(R.id.ssid),KEY_SSID, R.string.summary_ssid);
+        fillDescription(findViewById(R.id.bssid),KEY_BSSID, R.string.summary_bssid);
     }
 
     @Override
@@ -33,10 +132,8 @@ public class TaskerEditActivity extends AppCompatActivity {
         /*
          * Bundle that Tasker / Locale will send
          */
-        final Bundle resultBundle =
-                new Bundle();
-        resultBundle.putBoolean(EXTRA_MASTER, state);
-        resultIntent.putExtra(EXTRA_BUNDLE, resultBundle);
+        prefBundle.putBoolean(KEY_MASTER, state);
+        resultIntent.putExtra(EXTRA_BUNDLE, prefBundle);
 
         /*
          * The blurb is concise status text to be displayed in the host's UI.
--- app/src/main/java/eu/chylek/adam/fakewifi/TaskerReceiver.java
+++ app/src/main/java/eu/chylek/adam/fakewifi/TaskerReceiver.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 
 /**
  * Created by Adam Chýlek on 09.09.2017.
@@ -13,11 +14,23 @@ public class TaskerReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if ("com.twofortyfouram.locale.intent.action.FIRE_SETTING".equals(intent.getAction())){
-            boolean state = intent.getBundleExtra(TaskerEditActivity.EXTRA_BUNDLE).getBoolean(TaskerEditActivity.EXTRA_MASTER);
+            Bundle bundle = intent.getBundleExtra(TaskerEditActivity.EXTRA_BUNDLE);
+            boolean state = bundle.getBoolean(TaskerEditActivity.KEY_MASTER);
             SharedPreferences.Editor editor = context.getSharedPreferences(Utils.PREFERENCE_NAME, Context.MODE_PRIVATE).edit();
             editor.putBoolean("master", state);
+            putIfSet(TaskerEditActivity.KEY_BSSID, "bssid", bundle, editor);
+            putIfSet(TaskerEditActivity.KEY_SSID, "ssid", bundle, editor);
+            putIfSet(TaskerEditActivity.KEY_MAC, "mac", bundle, editor);
             editor.commit(); // do not use apply, otherwise the Xposed part of the module won't update its settings
             Utils.fixPreferencePermission(context);
+        }
+    }
+
+    public void putIfSet(String key, String prefKey, Bundle bundle, SharedPreferences.Editor editor)
+    {
+        String s = bundle.getString(key,"");
+        if (!"".equals(s)){
+            editor.putString(prefKey,s);
         }
     }
 }
